906b037e1ee0028634f48d64fa1008b243b020eb
Dastan <dassu8457@gmail.com>
2021-10-10 22:58:11 +0600

    add procfil

--- .Procfile
+++ .Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run:aipresent
+web: gunicorn run:app
--- run.py
+++ run.py
@@ -3,15 +3,15 @@ from flask import render_template
 from flask import send_from_directory
 
 app = Flask(__name__)
-try:
-    app.config.from_pyfile('config.py')
-    host = app.config['HOST']
-    debug = app.config['DEBUG']
-    port = app.config['PORT']
-except Exception:
-    host = []
-    debug = True
-    port = 8000
+# try:
+#     app.config.from_pyfile('config.py')
+#     host = app.config['HOST']
+#     debug = app.config['DEBUG']
+#     port = app.config['PORT']
+# except Exception:
+#     host = []
+#     debug = True
+#     port = 8000
 
 @app.route('/')
 def index():
